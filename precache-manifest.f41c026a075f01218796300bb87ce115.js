65cbca5b049dec5d37a2bb67165440eb69b1be66
Noe Huaccharaque <noemk2@gmail.com>
2019-04-19 21:01:32 -0500

    Updates

---
+++ precache-manifest.f41c026a075f01218796300bb87ce115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "03da9487421207242401",
+    "url": "/rickandmorty/static/css/main.de52702d.chunk.css"
+  },
+  {
+    "revision": "03da9487421207242401",
+    "url": "/rickandmorty/static/js/main.579ea6d6.chunk.js"
+  },
+  {
+    "revision": "a1b2f4593c2c28b002e8",
+    "url": "/rickandmorty/static/js/runtime~main.a1dd2e89.js"
+  },
+  {
+    "revision": "286ae9db63062a7d29a6",
+    "url": "/rickandmorty/static/css/2.daff3a7d.chunk.css"
+  },
+  {
+    "revision": "286ae9db63062a7d29a6",
+    "url": "/rickandmorty/static/js/2.b3de0af4.chunk.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/rickandmorty/static/media/logo.42d6efdb.png"
+  },
+  {
+    "revision": "37ab0aaa1e3aeb3d39ff1646663bfe25",
+    "url": "/rickandmorty/index.html"
+  }
+];
